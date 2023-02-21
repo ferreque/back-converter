87db0fd790c5872592ff86c01d0531ef052f7ab0
Fernando Requena <fernandorequenaserra@gmail.com>
2023-02-21 20:16:57 +0100

    vercel2

--- index.js
+++ index.js
@@ -18,36 +18,34 @@ mongoose
 const MOCK = require("./Mocks/mocks");
 const Conversion = require("./Models/conversions");
 
-app
-  .route("/data")
-  .get(async (req, res) => {
-    const allConvesiones = await Conversion.find();
-    res.status(200).json(allConvesiones);
-  })
-  .post(async (req, res) => {
-    const { body } = req;
-    try {
-      const newConvesion = new Conversion(body);
-      console.log(newConvesion);
-      await newConvesion.save();
-      res.status(200).json(newConvesion);
-    } catch (error) {
-      res.status(400).json(error);
-      console.log(error);
-    }
-  })
-  .delete(async (req, res) => {
-    const { id } = req.body;
-    console.log(id);
-    try {
-      const deletConv = await Conversion.findOneAndDelete({ id });
-      console.log(deletConv);
-      res.status(200).json(deletConv);
-    } catch (error) {
-      res.status(400).json({ error: true, message: error });
-      console.log(error);
-    }
-  });
+app.route("/data").get(async (req, res) => {
+  const allConvesiones = await Conversion.find();
+  res.status(200).json(allConvesiones);
+});
+// .post(async (req, res) => {
+//   const { body } = req;
+//   try {
+//     const newConvesion = new Conversion(body);
+//     console.log(newConvesion);
+//     await newConvesion.save();
+//     res.status(200).json(newConvesion);
+//   } catch (error) {
+//     res.status(400).json(error);
+//     console.log(error);
+//   }
+// })
+// .delete(async (req, res) => {
+//   const { id } = req.body;
+//   console.log(id);
+//   try {
+//     const deletConv = await Conversion.findOneAndDelete({ id });
+//     console.log(deletConv);
+//     res.status(200).json(deletConv);
+//   } catch (error) {
+//     res.status(400).json({ error: true, message: error });
+//     console.log(error);
+//   }
+// });
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ` + PORT);
