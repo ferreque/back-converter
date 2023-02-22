de9bb8733e4678db3bc5609b0023d211aafdcf8a
Fernando Requena <fernandorequenaserra@gmail.com>
2023-02-22 09:44:49 +0100

    git base

--- index.js
+++ index.js
@@ -20,36 +20,37 @@ const Conversion = require("./Models/conversions");
 
 app.route("/").get(async (req, res) => {
   try {
-    const allConvesiones = await Conversion.find();
-    res.status(200).json(allConvesiones);
+    res.status(200).json({ message: "OK" });
+    // const allConvesiones = await Conversion.find();
+    // res.status(200).json(allConvesiones);
   } catch (error) {
     res.status(400).json({ error: true, message: error });
   }
 });
-app.route("/data").post(async (req, res) => {
-  const { body } = req;
-  try {
-    const newConvesion = new Conversion(body);
-    console.log(newConvesion);
-    await newConvesion.save();
-    res.status(200).json(newConvesion);
-  } catch (error) {
-    res.status(400).json({ error: true, message: error });
-    console.log(error);
-  }
-});
-app.route("/data").delete(async (req, res) => {
-  const { id } = req.body;
-  console.log(id);
-  try {
-    const deletConv = await Conversion.findOneAndDelete({ id });
-    res.status(200).json(deletConv);
-  } catch (error) {
-    res.status(400).json({ error: true, message: error });
-    console.log(error);
-  }
-});
+// app.route("/data").post(async (req, res) => {
+//   const { body } = req;
+//   try {
+//     const newConvesion = new Conversion(body);
+//     console.log(newConvesion);
+//     await newConvesion.save();
+//     res.status(200).json(newConvesion);
+//   } catch (error) {
+//     res.status(400).json({ error: true, message: error });
+//     console.log(error);
+//   }
+// });
+// app.route("/data").delete(async (req, res) => {
+//   const { id } = req.body;
+//   console.log(id);
+//   try {
+//     const deletConv = await Conversion.findOneAndDelete({ id });
+//     res.status(200).json(deletConv);
+//   } catch (error) {
+//     res.status(400).json({ error: true, message: error });
+//     console.log(error);
+//   }
+// });
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ` + PORT);
 });
