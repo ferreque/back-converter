56719eaa2665ab8581b9445a642aa6889635c7ee
Fernando Requena <fernandorequenaserra@gmail.com>
2023-02-22 15:16:51 +0100

    require true

--- models/data.js
+++ models/data.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-  id: String,
-  inputValor: String,
-  uniti2: String,
-  valor: String,
-  uniti: String,
+  id: { type: String, require: true },
+  inputValor: { type: String, require: true },
+  uniti2: { type: String, require: true },
+  valor: { type: String, require: true },
+  uniti: { type: String, require: true },
 });
 
 const Product = mongoose.model("products", productSchema);
